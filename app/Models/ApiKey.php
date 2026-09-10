ba761d7965ec56837166db17695b55ae5429aa3c
Anton Shamordin <shamordinanton@gmail.com>
2026-09-10 18:53:49 +0300

    create ApiKeyService with create method

--- app/Models/ApiKey.php
+++ app/Models/ApiKey.php
@@ -9,6 +9,15 @@ use App\Models\Server;
 
 class ApiKey extends Model 
 {
+    protected $fillable = [
+        'server_id',
+        'name',
+        'identifier',
+        'key_hash',
+        'active',
+        'last_used_at',
+    ];
+
     public function server(): BelongsTo 
     {
         return $this->belongsTo(Server::class);
